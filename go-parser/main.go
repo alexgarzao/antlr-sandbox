01fd94acad33cf45b858d862321a42d6fd88524d
Alex S. Garzão <alexgarzao@gmail.com>
2020-05-06 22:51:19 -0300

    Parser to read go programs

---
+++ go-parser/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alexgarzao/antlr-sandbox/go-parser/parser"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+)
+
+var hello = `
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printlm("Hello")
+}
+`
+
+func main() {
+	fmt.Println("1 + 2 * 3 =", calc(hello))
+}
+
+// calc takes a string expression and returns the evaluated result.
+func calc(input string) int {
+	// Setup the input
+	is := antlr.NewInputStream(input)
+
+	// Create the Lexer
+	lexer := parser.NewGoLexer(is)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Create the Parser
+	p := parser.NewGoParser(stream)
+
+	// Finally parse the expression (by walking the tree)
+	var listener goListener
+	antlr.ParseTreeWalkerDefault.Walk(&listener, p.SourceFile())
+
+	return 1
+}
+
+type goListener struct {
+	*parser.BaseGoParserListener
+}
+
+func (l *goListener) VisitTerminal(node antlr.TerminalNode) {
+	fmt.Println("#### ", node.GetText())
+}
